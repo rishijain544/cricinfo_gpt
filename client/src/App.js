3451f5eb3de4591321bd858b0b18196cf52fef6b
rishijain544 <rishijain30a@gmail.com>
2026-03-15 16:54:38 +0530

    Integrate Adsterra ads script

--- client/src/App.js
+++ client/src/App.js
@@ -37,11 +37,6 @@ function App() {
       
       <LiveScores />
       
-      <div className="ad-container top-banner">
-        {/* Placeholder for Top Banner Ad */}
-        <div className="ad-placeholder">PROMOTED AD SLOT</div>
-      </div>
-
       <main className="main-content">
         <QuizSidebar />
         <ChatBox isApiOnline={isApiOnline} />
